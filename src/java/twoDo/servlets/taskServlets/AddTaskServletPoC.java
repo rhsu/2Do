e7d9431fa0e2bcd56f48a152c3ca23bf5a3cb373
robert <rhsu123@gmail.com>
2015-01-27 21:06:38 -0500

    #12: removed some old servlets

--- src/java/twoDo/servlets/taskServlets/AddTaskServletPoC.java
+++ src/java/twoDo/servlets/taskServlets/AddTaskServletPoC.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AddTaskServlet extends HttpServlet 
+public class AddTaskServletPoC extends HttpServlet 
 {
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException 
