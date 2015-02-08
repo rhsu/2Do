4736a217c2c15b11f75db5db122370018a23cb8e
robert <rhsu123@gmail.com>
2015-02-08 13:46:08 -0500

    Adding in sqlTest

---
+++ src/java/twoDo/test/SqlTest.java
@@ -0,0 +1,59 @@
+package twoDo.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SqlTest 
+{
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+	
+	public SqlTest()
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/2Do",
+							"root",
+							"password");
+			st = con.createStatement();
+		}
+		catch(ClassNotFoundException | SQLException ex)
+		{
+			System.out.println(ex);
+		}
+	}
+	
+	public void getData()
+	{
+		try
+		{
+			String query = "SELECT * FROM Task";
+			rs = st.executeQuery(query);
+			
+			System.out.println("Records from database");
+			
+			while(rs.next())
+			{
+				String name = rs.getString("Name");
+				String content = rs.getString("Content");
+				System.out.println(name + " " + content);
+			}
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex);
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		SqlTest test = new SqlTest();
+		test.getData();
+	}
+}
