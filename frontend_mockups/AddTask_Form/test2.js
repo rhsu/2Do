38aaf53459aecadeb73e5d6104879ba72f07ae6d
robert <rhsu123@gmail.com>
2015-04-20 20:58:10 -0400

    committing for backup things seem to work

---
+++ frontend_mockups/AddTask_Form/test2.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    var state = 'initial';
+    
+	var display = function () {
+        
+        if (state === 'form') {
+            $('#initialPageState_AddTaskForm').hide();
+            $('#formPageState_AddTaskForm').show();
+        }
+        else if (state === 'initial') {
+            $('#initialPageState_AddTaskForm').show();
+            $('#formPageState_AddTaskForm').hide();
+        }
+        else {
+            console.log(state);
+        }
+    };
+    
+    display();
+    
+    // event listeners
+    
+    $('#btnAddTask_AddTaskInitial').click(function () {
+        state = 'form';
+        display();
+    });
+    
+    $ ('#btnX_AddTaskForm').click(function () {
+        state = 'initial';
+        display();
+    });
+});
