ef03d65a6a7cb953aee08a3a273c3fe2b436d27b
robert <rhsu123@gmail.com>
2015-07-10 20:06:22 -0400

    Not sure how to test the database service

--- test/twoDo/services/UserTaskServiceTest.java
+++ test/twoDo/services/UserTaskServiceTest.java
@@ -4,9 +4,17 @@ import twoDo.TwoDoTestCase;
 
 public class UserTaskServiceTest extends TwoDoTestCase
 {
+	protected TaskService service;
+	
 	public UserTaskServiceTest(String method)
 	{
 		super(method);
+	}
+	
+	@Override
+	public void setUp()
+	{
+		service = new UserTaskService();
 	}
 	
 	public void testInsertTask()
