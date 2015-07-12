4fe2dfa14673084ca9557bead5f7518a0dfebb72
robert <rhsu123@gmail.com>
2015-07-12 14:29:57 -0400

    renamed a package

--- src/java/twoDo/services/UserTaskService.java
+++ src/java/twoDo/services/UserTaskService.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import twDo.dataLayer.DataLayer;
+import twoDo.dataLayer.DataLayer;
 import twoDo.models.Task;
 import twoDo.models.UserTask;
 
